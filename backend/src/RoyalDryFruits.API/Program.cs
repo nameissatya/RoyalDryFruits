66e60049451c160e254b95c212437b56499660c7
Venkata Satyanarayana Agravarapu <satyanarayanaagravarapu@gmail.com>
2026-08-15 15:33:11 +0530

    Fix: Rename port to dbPort to avoid variable name conflict

--- backend/src/RoyalDryFruits.API/Program.cs
+++ backend/src/RoyalDryFruits.API/Program.cs
@@ -40,8 +40,8 @@ if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("post
 {
     var uri = new Uri(connectionString);
     var userInfo = uri.UserInfo.Split(':');
-    var port = uri.Port > 0 ? uri.Port : 5432;
-    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var dbPort = uri.Port > 0 ? uri.Port : 5432;
+    connectionString = $"Host={uri.Host};Port={dbPort};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
